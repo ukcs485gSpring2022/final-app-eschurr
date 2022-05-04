026ee7d2c6cbd1b688bba9791fe018db2c826365
Eric Schurr <82740749+eschurr@users.noreply.github.com>
2022-05-03 20:22:22 -0400

    Replace the current TipView with a class with CustomFeaturedContentView that subclasses OCKFeaturedContentView (#16)

---
+++ OCKSample/MainTabs/Care/CustomFeaturedTaskView.swift
@@ -0,0 +1,45 @@
+//
+//  CustomFeaturedTaskView.swift
+//  OCKSample
+//
+//  Created by Eric Schurr on 5/3/22.
+//  Copyright © 2022 Network Reconnaissance Lab. All rights reserved.
+//
+
+import UIKit
+import CareKitUI
+import CareKit
+
+/// A simple subclass to take control of what CareKit already gives us.
+ class CustomFeaturedContentView: OCKFeaturedContentView {
+     var url: URL?
+
+     // Need to override so we can become delegate when the user taps on card
+     override init(imageOverlayStyle: UIUserInterfaceStyle = .unspecified) {
+         super.init(imageOverlayStyle: imageOverlayStyle)
+         // Need to become a delegate so we know when view is tapped.
+         self.delegate = self
+     }
+
+     convenience init(url: String, imageOverlayStyle: UIUserInterfaceStyle = .unspecified) {
+         self.init(imageOverlayStyle: imageOverlayStyle) // This calls your local init
+         // tODO: 1 - Need to turn the url string into a real URL using URL(string: String)
+         self.url = URL(string: url)
+         // Need to become a delegate so we know when view is tapped.
+         self.delegate = self
+     }
+ }
+
+ /// Need to conform to delegate in order to be delegated to.
+ extension CustomFeaturedContentView: OCKFeaturedContentViewDelegate {
+
+     func didTapView(_ view: OCKFeaturedContentView) {
+         // When tapped open a URL.
+         guard let url = url else {
+             return
+         }
+         DispatchQueue.main.async {
+             UIApplication.shared.open(url)
+         }
+     }
+ }
